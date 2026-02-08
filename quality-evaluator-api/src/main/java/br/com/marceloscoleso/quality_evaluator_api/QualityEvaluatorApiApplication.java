bc4e3257d7aa2c352221fc1b2a2b661bad73b836
MarceloScoleso <marcelo.scolesojr@gmail.com>
2026-02-08 04:09:36 -0300

    chore: criação inicial do projeto Spring Boot

---
+++ quality-evaluator-api/src/main/java/br/com/marceloscoleso/quality_evaluator_api/QualityEvaluatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marceloscoleso.quality_evaluator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityEvaluatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityEvaluatorApiApplication.class, args);
+	}
+
+}
---
+++ quality-evaluator-api/src/test/java/br/com/marceloscoleso/quality_evaluator_api/QualityEvaluatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marceloscoleso.quality_evaluator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityEvaluatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

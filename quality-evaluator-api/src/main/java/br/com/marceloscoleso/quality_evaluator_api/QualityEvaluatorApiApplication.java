dce9608ac70dba2853c2ac1b5fcde522de282d24
MarceloScoleso <marcelo.scolesojr@gmail.com>
2026-03-25 00:13:27 -0300

    fix: remove dotenv

--- quality-evaluator-api/src/main/java/br/com/marceloscoleso/quality_evaluator_api/QualityEvaluatorApiApplication.java
+++ quality-evaluator-api/src/main/java/br/com/marceloscoleso/quality_evaluator_api/QualityEvaluatorApiApplication.java
@@ -9,14 +9,7 @@ public class QualityEvaluatorApiApplication {
 
     public static void main(String[] args) {
         
-        Dotenv dotenv = Dotenv.load();
 
-        
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
 
        

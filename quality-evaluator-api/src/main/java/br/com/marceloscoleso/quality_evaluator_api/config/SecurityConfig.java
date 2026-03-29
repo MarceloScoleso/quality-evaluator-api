b8d303b1dd08271e6b97e5c7363adceb8c477932
MarceloScoleso <marcelo.scolesojr@gmail.com>
2026-03-29 08:42:31 -0300

    fix: CORS

--- quality-evaluator-api/src/main/java/br/com/marceloscoleso/quality_evaluator_api/config/SecurityConfig.java
+++ quality-evaluator-api/src/main/java/br/com/marceloscoleso/quality_evaluator_api/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.com.marceloscoleso.quality_evaluator_api.config;
 
 import br.com.marceloscoleso.quality_evaluator_api.security.JwtAuthenticationFilter;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -67,4 +69,24 @@ public class SecurityConfig {
             AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+    org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
+
+    config.setAllowCredentials(true);
+    config.setAllowedOriginPatterns(List.of(
+        "https://code-quality-evaluator-frontend-ppfq4omrd.vercel.app",
+        "https://code-quality-evaluator-frontend.vercel.app"
+    ));
+
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+
+    org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+            new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", config);
+    return source;
+}
 }
